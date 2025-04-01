33074090a8214a33b2dc1379a706d753021f542b
Filip Buljan <filipbuljan2002@gmail.com>
2025-04-01 22:30:06 +0200

    Add argparse support for YAML config in training script

--- train.py
+++ train.py
@@ -1,9 +1,14 @@
 import torch
 import yaml
+import argparse
 from ultralytics import YOLO
 
 def main():
-    with open("config.yaml", "r") as f:
+    parser = argparse.ArgumentParser(description="Train YOLO model with YAML config")
+    parser.add_argument("--config", type=str, required=True, help="Path to config YAML file")
+    args = parser.parse_args()
+
+    with open(args.config, "r") as f:
         config = yaml.safe_load(f)
 
     model = YOLO(config["model"])
